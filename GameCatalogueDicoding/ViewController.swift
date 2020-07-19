506420f0a3b8afa34dea6aa3c87ab6df3d1a37d8
Kukuh Alfian <kukuh@majoo.id>
2020-07-19 10:01:59 +0700

    Initial Commit

---
+++ GameCatalogueDicoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCatalogueDicoding
+//
+//  Created by Kalfian on 19/07/20.
+//  Copyright © 2020 Kalfian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
